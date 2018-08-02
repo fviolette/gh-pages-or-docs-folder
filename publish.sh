3b67e4ed9bd7953d186bc8e724651b780deb0279
François Violette <francoisviolette@gmail.com>
2018-08-02 08:22:27 +0200

    Update DITA-OT version

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 wget https://github.com/dita-ot/dita-ot/releases/download/2.3.3/dita-ot-2.3.3.zip
 unzip dita-ot-2.3.3.zip 
 
-cd dita-ot-2.3.3/
+cd dita-ot-3.1.1/
 bin/ant -f integrator.xml
 cd ..
 
 rm -rf out
 
-dita-ot-2.3.3/bin/dita -i $DITAMAP -f html5 -verbose
+dita-ot-3.1.1/bin/dita -i $DITAMAP -f html5 -verbose
